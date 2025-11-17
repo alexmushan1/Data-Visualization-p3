1188068133825f87e4743ee8722c35ff66a95fc7
Livianne Wong <127566773+LivianneW@users.noreply.github.com>
2025-11-17 10:42:25 -0800

    Update P3_plot4.py

--- P3_plot4.py
+++ P3_plot4.py
@@ -2,12 +2,6 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("parking_citations_2025_part2_datasd.csv")
-
-plt.scatter(df.index, df['vio_fine'])
-plt.xlabel('Index')
-plt.ylabel('vio_fine')
-plt.title('Scatter Plot of vio_fine by Index')
-plt.show()
+df = pd.read_csv("indicator_bacteria_tests_datasd.csv")
 
 
